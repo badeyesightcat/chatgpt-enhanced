495a2745a42df7bc3e4e1fb13d4cf450bf304e02
Beom Mee <whitebigbabybird@gmail.com>
2023-02-18 20:21:18 +0900

    Configure the starting point in server

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Configuration, OpenAIApi } = require('openai');
+const express = require('express');
+
+const configuration = new Configuration({
+  organization: 'org-xsDpA4JqpAo8U4iJOl9GHkR9',
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+// create a simple express api that calls the function
+const app = express();
+const port = 3000;
+
+app.post('/', async (req, res) => {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: 'Say this is a test',
+    max_tokens: 7,
+    temperature: 0,
+  });
+  console.log(response);
+  res.json({
+    data: response.data,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`example app listening at http://localhost:${port}`);
+});
