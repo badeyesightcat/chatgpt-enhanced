74f6beabdeda48352185b210d9becf96b243eb60
Beom Mee <whitebigbabybird@gmail.com>
2023-02-19 13:15:38 +0900

    Make the chat message item into a component

---
+++ client/src/components/ChatMessageItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="chat-message-item">
+    <div class="chat-message-avatar">me</div>
+    <div class="chat-message-content">who are you by the way?</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ChatMessageItem',
+  props: {
+    message: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
+<style scoped>
+.chat-message-item {
+  display: flex;
+  padding: 1.5rem 3rem;
+  gap: 1rem;
+  justify-content: center;
+}
+.chat-message-avatar {
+  border-radius: 1rem;
+  width: 2.5rem;
+  height: 2.5rem;
+  background-color: aliceblue;
+}
+.chat-message-content {
+  flex: 0 0 44.5rem;
+  color: #fff;
+}
+</style>
