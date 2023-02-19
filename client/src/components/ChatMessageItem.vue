1c587017e0c0381aaf258d875403bc89900aa705
Beom Mee <whitebigbabybird@gmail.com>
2023-02-19 13:35:53 +0900

    Develop ChatMessageItem to get props about the author

--- client/src/components/ChatMessageItem.vue
+++ client/src/components/ChatMessageItem.vue
@@ -1,16 +1,33 @@
 <template>
-  <div class="chat-message-item">
-    <div class="chat-message-avatar">me</div>
-    <div class="chat-message-content">who are you by the way?</div>
+  <div class="chat-message-item" :class="author === 'ai' && 'ai-answered'">
+    <div class="chat-message-avatar">
+      <ChatGptAvatar v-if="author === 'ai'" />
+      <template v-else>me</template>
+    </div>
+    <div class="chat-message-content">
+      {{ author === 'ai' ? 'I am an AI' : 'who are you by the way?' }}
+    </div>
   </div>
 </template>
 <script>
+import ChatGptAvatar from './ChatGptAvatar.vue';
+
 export default {
   name: 'ChatMessageItem',
+  components: {
+    ChatGptAvatar,
+  },
   props: {
-    message: {
+    content: {
       type: Object,
-      default: () => ({}),
+      default: () => ({
+        avatar: String,
+        message: String,
+      }),
+    },
+    author: {
+      type: String,
+      default: '',
     },
   },
 };
@@ -31,5 +48,16 @@ export default {
 .chat-message-content {
   flex: 0 0 44.5rem;
   color: #fff;
+}
+.chat-message-item.ai-answered {
+  background-color: rgba(68, 70, 84, 1);
+}
+.chat-message-item.ai-answered .chat-message-avatar {
+  padding: 0.5rem;
+  background-color: rgb(16, 163, 127);
+}
+.chat-message-item.ai-answered svg {
+  width: 1.5rem;
+  height: 1.5rem;
 }
 </style>
